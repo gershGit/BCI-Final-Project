c224811ff2e5d92c882034cd681ced901c225428
Garret Gershmel <gershmail1@gmail.com>
2019-04-11 10:21:52 -0500

    Added basic program structure
    +Added the steps that must be taken each frame to play the game. Steps should be filled out in separate functions (possibly in other files when applicable)

--- JS_Project/src/main.js
+++ JS_Project/src/main.js
@@ -3,3 +3,13 @@
 window.onload = function init() {
     console.log("Hello world!");
 }
+
+function mainLoop(){
+    //TODO gather BCI data                                  <Make separate javascript file for this>
+    //TODO apply BCI to difficulty
+    //TODO calculate input
+    //TODO calculate physics
+    //TODO calculate collisions / points / delete objects   <Points in HTML / CSS? >
+    //TODO render geometry
+    //TODO render UI if not in html
+}
