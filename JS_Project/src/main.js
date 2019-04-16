9dd48f90eaaa0a048c4117d1842c0bf4fe13d39f
Garret Gershmel <gershmail1@gmail.com>
2019-04-16 14:53:12 -0500

    FPS and physics
    +Added FPS look controls to the camera using the mouse
    +Added physics to all objects of type 1
    +Added the ability to launch cubes every 1 second using the mouse click
    +Canvas now locks mouse pointer
    
    !Camera control gets inverted when the user has rotated too much to the left or right

--- JS_Project/src/main.js
+++ JS_Project/src/main.js
@@ -5,8 +5,8 @@
 //Global variables to hold webGL objects
 var canvas;
 var gl;
+var locked = false;
 
-//Variables for the shaders
 //Uniform IDs
 var model_id;
 var view;
@@ -19,25 +19,53 @@ var vTexCoord;
 //Shader Program
 var vProgram;
 //View and perspective matrices
-var viewMat = lookAt(vec3(0,0,1.5), vec3(0,0,0), vec3(0,1,0));
-var perspMat = perspective(120, 1, 0.1, 16.0);
+var sensitivity = 0.3;
+var camPos = vec3(0.0,0.2,0);
+var camDir = vec3(0.0,0.0,-1.0);
+var perspMat = perspective(70, 1, 0.1, 48.0);
 
 //Time Control
 var lastTime;
 var deltaTime;
+var scaledTime;
 var paused = false;
 var timeSpeed = 1.0; 
+var timeSinceLastLaunch = 0.0; //Tracking variable for fire rate
 
 //Global data
 var objectList = [];
 
+//Setup on load
 window.onload = function init() {
     canvas = document.getElementById("gl-canvas"); //Get canvas from html
+
+    //Pointer locking
+    canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+    document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+    document.addEventListener('pointerlockchange', lockChangeAlert, false);
+    document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+    //Handles pointer lock and launching of projectiles on mouse click
+    canvas.onclick = function() {
+        if (locked) {
+            //Check if enough time has passed to allow for a new ball to be launched
+            if (timeSinceLastLaunch > 1.0) {
+                var newProjectile = buildCube(1);
+                newProjectile.scale = vec3(0.2, 0.2, 0.2);
+                newProjectile.position = add(vec3(0,0.2,0), scale(2,camDir));
+                newProjectile.velocity = scale(15.0, camDir);
+                objectList.push(newProjectile);
+                timeSinceLastLaunch = 0.0;
+            }
+
+        }
+        canvas.requestPointerLock();
+    };
+
     gl = WebGLUtils.setupWebGL(canvas);	//Get gl context from Common directory
     if ( !gl ) { alert( "WebGL failed to load" ); }	//Error handle when WebGL fails
 
     gl.viewport(0, 0, canvas.width, canvas.height);	//Set the viewport equal to the canvas
-    gl.clearColor(0.1, 0.2, 0.3, 1.0);	//Set the clear color to white
+    gl.clearColor(0.7, 1.0, 1.0, 1.0);	//Set the clear color to white
     gl.enable(gl.DEPTH_TEST); //Turns on depth testing
 
     vProgram = initShaders(gl, "vertex-shader", "fragment-shader");	//Compile the shader program
@@ -55,27 +83,72 @@ window.onload = function init() {
     fac_vPosition = vPosition;
     fac_vTexCoord = vTexCoord;
 
-    console.log("Hello world!");
     objectList.push(buildSquare(0));
+    objectList[0].position = subtract(objectList[0].position, vec3(0.0,0.25,0.0));
+    objectList[0].scale = vec3(3,3,3);
+
+    lastTime = Date.now();
     mainLoop();
 }
 
+//Runs continuously and handles per frame function calls
 function mainLoop(){
+    //Handle time calculations
+    deltaTime = (Date.now() - lastTime) / 1000.0;
     lastTime = Date.now();
+    scaledTime = deltaTime * timeSpeed;
+    timeSinceLastLaunch += deltaTime;
+
     //TODO gather BCI data                                  <Make separate javascript file for this>
     //TODO apply BCI to difficulty
-    //TODO calculate input
-    //TODO calculate physics
-    //TODO calculate collisions / points / delete objects   <Points in HTML / CSS? >
+    updatePhysics();
+    calculateCollisions();
+    //TODO                                                  <Points in HTML / CSS? >
     render();
     //TODO render UI if not in html
     requestAnimationFrame(mainLoop);
 }
 
+//Updates the position of all objects using physics
+function updatePhysics(){
+    for (i=0; i<objectList.length; i++) {
+        //1 for projectiles
+        if (objectList[i].type_id == 1) {
+            //Calculate the change in position
+            var deltaPos = scale(deltaTime, add( objectList[i].velocity , scale(deltaTime*0.5, vec3(0,-9.8,0))) );
+            //console.log(deltaPos[0], deltaPos[1], deltaPos[2]);
+            objectList[i].position = add(objectList[i].position, deltaPos);
+            objectList[i].velocity = add(objectList[i].velocity, scale(deltaTime, vec3(0,-9.8,0)));
+        }
+        //2 for enemies
+        else if (objectList[i].type_id == 2){
+
+        }
+    }
+}
+
+//TODO calculate collisions / points / delete objects
+function calculateCollisions(){
+    for (i=0; i<objectList.length; i++) {
+        //1 for projectiles
+        if (objectList[i].type_id == 1) {
+            //Calculate the change in position
+            if (objectList[i].position[1] < -5) {
+                objectList.splice(i,1); //Remove projectiles that have fallen out of screen
+            }
+        }
+        //2 for enemies
+        else if (objectList[i].type_id == 2){
+
+        }
+    }
+}
+
+//Renders all objects in scene
 function render(){
     //Clear the canvas
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-    gl.uniformMatrix4fv(view, gl.TRUE, flatten(viewMat));
+    gl.uniformMatrix4fv(view, gl.TRUE, flatten(lookAt(camPos, add(camPos, camDir), vec3(0,1,0))));
     gl.uniformMatrix4fv(perspective_id, gl.TRUE, flatten(perspMat));
     //Render each object in the master list
     for (i=0; i<objectList.length; i++) {
@@ -83,12 +156,15 @@ function render(){
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, objectList[i].iBuffer);
         gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 24, 0);
         gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 24, 16 );
+
+        //TODO send in color data
         gl.uniform1i(tPosition, objectList[i].texture);
         gl.uniformMatrix4fv(model_id, gl.TRUE, flatten( getTransform(objectList[i].position, objectList[i].rotation, objectList[i].scale) ));
         gl.drawElements(gl.TRIANGLES, objectList[i].indexCount, gl.UNSIGNED_SHORT, 0);
     }
 }
 
+//Returns a transformation matrix from a position, rotation, and scale
 function getTransform(t, r, s) {
     var mat = scalem(s[0], s[1], s[2]);
     mat = mult (rotate(r[0], vec3(1,0,0)) , mat);
@@ -96,4 +172,33 @@ function getTransform(t, r, s) {
     mat = mult (rotate(r[2], vec3(0,0,1)) , mat);
     mat = mult (translate(t[0], t[1], t[2]) , mat);
     return mat;
+}
+
+//From Mozilla documentation: https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+function lockChangeAlert() {
+    if (document.pointerLockElement === canvas ||
+        document.mozPointerLockElement === canvas) {
+      locked = true;
+      document.addEventListener("mousemove", updatePosition, false);
+    } else {
+      locked = false;
+      document.removeEventListener("mousemove", updatePosition, false);
+    }
+  }
+
+//Updates the camera rotation based on the mouse movement
+function updatePosition(e) {
+    //TODO mouse movement gets inverted Y after turning around
+    var xRot = rotate(-e.movementX, vec3(0,1,0));
+    var yRot = rotate(-e.movementY, vec3(1,0,0));
+    camDir = applyMatrix(camDir, mult(xRot, yRot));
+}
+
+//Matrix vector multiplication
+function applyMatrix(v, m) {
+    var r = vec3(0,0,0);
+    r[0] = (m[0][0] * v[0]) + (m[0][1] * v[1]) + (m[0][2] * v[2]);
+    r[1] = (m[1][0] * v[0]) + (m[1][1] * v[1]) + (m[1][2] * v[2]);
+    r[2] = (m[2][0] * v[0]) + (m[2][1] * v[1]) + (m[2][2] * v[2]);
+    return r;
 }
--- JS_Project/src/meshFactory.js
+++ JS_Project/src/meshFactory.js
@@ -2,15 +2,17 @@ var fac_vPosition;
 var fac_vTexCoord;
 
 class GameObject {
-    constructor(id) {
-        this.id = id;
+    constructor(type_id) {
+        this.type_id = type_id;
         this.vBuffer;
         this.iBuffer;
         this.indexCount = 0;
         this.texture;
-        this.position = translate(0, 0, 0);
-        this.rotation = translate(0,0,0);
-        this.scale = scalem(1,1,1);
+        this.position = vec3(0, 0, 0);
+        this.rotation = vec3(0,0,0);
+        this.scale = vec3(1,1,1);
+        this.velocity = vec3(0,0,0);
+        this.color = vec3(1,1,1);
     }
 }
 
@@ -21,8 +23,27 @@ var squareVertices = [-1, 0, -1, 1, 0,0,
      1, 0, -1, 1, 1,0];
 var squareIndices = [0,1,2,0,3,2];
 
+//Mesh information for a cube
+var cubeVertices =  [
+    -1, 1, -1, 1, 0,0,
+    -1, 1, 1, 1, 0,1,
+    1, 1, 1, 1, 1,1, 
+    1, 1, -1, 1, 1,0, 
+    
+    -1, -1, -1, 1, 0,0,
+    -1, -1, 1, 1, 0,1,
+    1, -1, 1, 1, 1,1, 
+    1, -1, -1, 1, 1,0];
+var cubeIndices = [0,1,2,0,3,2,
+                    4,5,6,4,7,6,
+                    0,1,4,5,4,1,
+                    3,2,6,7,6,3,
+                    0,3,4,3,4,7,
+                    1,2,5,5,6,2];
+
 function buildSquare(id){
     var square = new GameObject(id);
+    square.type_id = id;
     square.vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, square.vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(squareVertices), gl.STATIC_DRAW);
@@ -39,4 +60,25 @@ function buildSquare(id){
     gl.enableVertexAttribArray( fac_vTexCoord );
 
     return square;
+}
+
+function buildCube(id){
+    var cube = new GameObject(id);
+    cube.type_id = id;
+    cube.vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cube.vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cubeVertices), gl.STATIC_DRAW);
+
+    cube.iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
+
+    cube.indexCount = cubeIndices.length;
+
+    gl.vertexAttribPointer( fac_vPosition, 4, gl.FLOAT, false, 24, 0 );
+    gl.enableVertexAttribArray( fac_vPosition );
+    gl.vertexAttribPointer( fac_vTexCoord, 2, gl.FLOAT, false, 24, 16 );
+    gl.enableVertexAttribArray( fac_vTexCoord );
+
+    return cube;
 }
