fb19bf38cd6ab7dd1fd8cf1f00fbc0e229ed19dd
JackHawb <40471522+JackHawb@users.noreply.github.com>
2019-04-30 22:20:27 -0500

    Update bciRetrieval.js

--- JS_Project/src/bciRetrieval.js
+++ JS_Project/src/bciRetrieval.js
@@ -1,16 +1,41 @@
 let openBCI = new OpenBCI();
 var connected = false;
 
+var freqVals = new Array(100);
+var initVals = false;
+var counter = 0;
+
 function onConnect(){
     openBCI.start();
     connected = true;
 }
 
 function getDifficulty(){
-    if (connected) {
-        //TODO sliding average
-        //TODO scaling of difficulty based on testing
-        return openBCI.getRelativeBandPower(0 , "alpha");
+    if(initVals == false){
+      initArr();
+      initVals = true;
     }
-    return .09;
+    if (connected) {
+        int currPower = openBCI.getRelativeBandPower(0 , "alpha");
+        var spot = counter%100;
+        freqVals[spot] = currPower;
+        var diff = avgVals();
+        counter++;
+        return diff;
+    }
+    return 0;
+}
+
+function initArr(){
+  for(int i = 0; i < 100; i++){
+    freqVals[i] = 1;
+  }
+}
+
+funciton avgVals(){
+  var sum = 0;
+  for(int i = 0; i < 100; i++){
+    sum += freqVals[i];
+  }
+  return sum/100;
 }
